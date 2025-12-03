caefdcdbcf877a37dbb74ee433168fe10d5f248e
Jesus Venegas <kasdeyaa@hotmail.com>
2025-12-03 11:27:12 -0800

    DASO-53: add debt via API works

--- frontend/src/app/debts/page.tsx
+++ frontend/src/app/debts/page.tsx
@@ -1,37 +1,25 @@
 import { Debt } from "@/types/debt";
 import DebtTable from "./components/DebtTable";
 
-export default function DebtsPage() {
-    const mockDebts: Debt[] = [
-        {
-            id: "1",
-            name: "RappiCard",
-            amount: 28600,
-            interestRate: 90,
-            minPayment: 17763.68,
-            dueDay: 28,
-        },
-        {
-            id: "2",
-            name: "Costco Banamex",
-            amount: 18854.43,
-            interestRate: 61,
-            minPayment: 430,
-            dueDay: 4,
-        },
-        {
-            id: "3",
-            name: "Nu",
-            amount: 1300,
-            interestRate: 69,
-            minPayment: 100,
-            dueDay: 27,
-        },
-    ];
+async function getDebts() {
+    const res = await fetch("http://localhost:5063/api/debts", {
+        cache: "no-store",
+    })
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch debts");
+    }
+
+    return res.json();
+}
+
+export default async function DebtsPage() {
+    const debts = await getDebts();
+
     return (
         <main className="text-white p-6">
             <h1 className="text-2xl font-bold mb-4">Debt List</h1>
-            <DebtTable debts={mockDebts} />
+            <DebtTable debts={debts} />
             <a href="/debts/add">Add New Debt</a>
         </main>
     )
